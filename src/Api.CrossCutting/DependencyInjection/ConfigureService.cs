6a839a421b38dc2b80e7d6f3ade943e1ea9fb114
Daisykmargo <dkmarguinha@gmail.com>
2021-06-23 10:49:25 -0300

    Aplicações de injeção dependência

---
+++ src/Api.CrossCutting/DependencyInjection/ConfigureService.cs
@@ -0,0 +1,15 @@
+using Api.Data.Repository;
+using Api.Domain.Interfaces;
+using Api.Domain.Interfaces.Services.User;
+using Api.Service.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.CrossCutting.DependencyInjection
+{
+    public class ConfigureService
+    {
+        public static void ConfigureDependenciesService (IServiceCollection serviceCollection){
+            serviceCollection.AddTransient<IUserService, UserService>();
+         }
+    }
+}
